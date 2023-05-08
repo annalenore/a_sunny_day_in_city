f28aa3b4fa408bd84d6901062d95e25f73ac6b4c
anna <114003841+annalenore@users.noreply.github.com>
2023-05-08 15:54:32 -0400

    8/8-3

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ function showPopup() {
 
       // Set the title and description in the popup
       document.getElementById('popupTitle').textContent = randomItem['Organization Name'];
-      document.getElementById('popupDescription').textContent = `Discipline: ${randomItem.Discipline}`;
-
+      document.getElementById('popupAddress').textContent = `Address: ${randomItem.Address}`;
+      document.getElementById('popupBorough').textContent = `Borough: ${randomItem.Borough}`;
+      document.getElementById('popupMainPhone').textContent = `MainPhone: ${randomItem.MainPhone}`;
+      
       // Show the popup
       document.getElementById('popup').style.display = 'block';
     });
