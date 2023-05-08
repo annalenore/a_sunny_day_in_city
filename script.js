de2ba5bf5d6768bb232d7deeb1b13e93744f6b4a
anna <114003841+annalenore@users.noreply.github.com>
2023-05-08 11:16:33 -0400

    3/8-3

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 function showPopup() {
-    // Load the JSON data
-    fetch('photography.json')
-      .then(response => response.json())
-      .then(data => {
-        // Randomly select an object from the data array
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomItem = data[randomIndex];
-  
-        // Set the title and description in the popup
-        document.getElementById('popupTitle').textContent = randomItem['Organization Name'];
-        document.getElementById('popupAddress').textContent = randomItem['Address'];
-        document.getElementById('popupBorough').textContent = randomItem['Bourough'];
-        document.getElementById('popupMainPhone').textContent = randomItem['Main Phone'];
-        document.getElementById('popupDiscipline').textContent = randomItem['Discipline'];
-        
+  // Load the JSON data
+  fetch('photography.json')
+    .then(response => response.json())
+    .then(data => {
+      // Randomly select an object from the data array
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomItem = data[randomIndex];
 
-  
-        // Show the popup
-        document.getElementById('popup').style.display = 'block';
-      });
-  }
-  
-  function closePopup() {
-    // Hide the popup
-    document.getElementById('popup').style.display = 'none';
-  }
+      // Set the title and description in the popup
+      document.getElementById('popupTitle').textContent = randomItem['Organization Name'];
+      document.getElementById('popupDescription').textContent = `Discipline: ${randomItem.Discipline}`;
+
+      // Show the popup
+      document.getElementById('popup').style.display = 'block';
+    });
+}
+
+function closePopup() {
+  // Hide the popup
+  document.getElementById('popup').style.display = 'none';
+}
 
   
   
