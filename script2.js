79d2c477b8b8af0e4a60ef4f94b7fe455db40782
anna <114003841+annalenore@users.noreply.github.com>
2023-05-09 13:06:37 -0400

    5/9-5

--- script2.js
+++ script2.js
@@ -60,3 +60,24 @@ function showPopup() {
         document.getElementById('popup2').style.display = 'block';
       });
   }
+
+  function showPopup3() {
+    // Load the JSON data
+    fetch('snowy.json')
+      .then(response => response.json())
+      .then(data => {
+        // Randomly select an object from the data array
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomItem = data[randomIndex];
+  
+        // Set the title and description in the popup
+        document.getElementById('popupTitle').textContent = randomItem['Organization Name'];
+        document.getElementById('popupAddress').textContent = `Address: ${randomItem.Address}`;
+        document.getElementById('popupBorough').textContent = `Borough: ${randomItem.Borough}`;
+        document.getElementById('popupMainPhone').textContent = `Main Phone: ${randomItem.MainPhone}`;
+        document.getElementById('popupDiscipline').textContent = `Discipline: ${randomItem.Discipline}`;
+        
+        // Show the popup
+        document.getElementById('popup3').style.display = 'block';
+      });
+  }
